e238b2f019044edb9c3297e6f490561f1c127285
Michael Kirschner <mjk.kirschner@gmail.com>
2021-08-07 13:30:26 -0400

    bit test 2

--- 6502_instruction_BIT_test.go
+++ 6502_instruction_BIT_test.go
@@ -18,3 +18,22 @@ func TestBITZP(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestBITABS(t *testing.T) {
+	sim := NewSimulatorFromInstructionData()
+
+	sim.Memory[0] = BIT_OPCODE_ABS
+	sim.Memory[1] = 5
+	sim.Memory[2] = 0
+	sim.Memory[5] = 128
+
+	sim.Run(1)
+
+	if sim.GetBit(REGISTER_STATUS, BITFLAG_STATUS_NEGATIVE) != true {
+		t.FailNow()
+	}
+	//confusing but - this is set if the AND of A and M is 0 (0 & 128 is 0)
+	if sim.GetBit(REGISTER_STATUS, BITFLAG_STATUS_ZERO) != true {
+		t.FailNow()
+	}
+}
